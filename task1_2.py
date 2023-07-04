0a991ed7338d2c943acdb2d81c2a04d5ae59888f
caregor <caregor@gmail.com>
2023-07-04 21:31:00 +0500

    Task 1_2 is Done.

--- task1_2.py
+++ task1_2.py
@@ -10,3 +10,24 @@
 Откажитесь от магических чисел
 В коде должны быть один input и один print
 """
+
+
+def process_number(number):
+    if 1 <= number <= 9:
+        square = number ** 2
+        return f"Введено однозначное число. Квадрат числа: {square}"
+    elif 10 <= number <= 99:
+        digit1 = number // 10
+        digit2 = number % 10
+        product = digit1 * digit2
+        return f"Введено двузначное число. Произведение цифр: {product}"
+    elif 100 <= number <= 999:
+        reversed_number = int(str(number)[::-1])
+        return f"Введено трехзначное число. Зеркальное отображение: {reversed_number}"
+    else:
+        return "Ошибка: Введено число, не попадающее в диапазон от 1 до 999."
+
+
+input_number = int(input("Введите число от 1 до 999: "))
+result = process_number(input_number)
+print(result)
