633afbf0be8b84c744b9f8465f167504b429ec96
caregor <caregor@gmail.com>
2023-07-04 10:09:46 +0500

    Task 1_3 is Done.

--- task1_3.py
+++ task1_3.py
@@ -3,3 +3,20 @@
 Задание №8
 Нарисовать в консоли ёлку спросив у пользователя количество рядов.
 """
+
+
+def draw_tree(levels):
+    for i in range(1, levels + 1):
+        print(" " * (levels - i), end="")
+        print("*" * (2 * i - 1))
+
+
+while True:
+    rows = int(input("Введите количество рядов ёлки: "))
+
+    if rows > 0:
+        break
+    else:
+        print("Ошибка: Количество рядов должно быть положительным числом.")
+
+draw_tree(rows)
