8bb4a384641938b8c5b992236b6f53f899f1c814
caregor <caregor@gmail.com>
2023-07-04 21:37:13 +0500

    Task 1_2 is Done.

--- task1_4.py
+++ task1_4.py
@@ -3,3 +3,14 @@
 Задание №9
 Выведите в консоль таблицу умножения от 2х2 до 9х10 как на школьной тетрадке.
 """
+
+for i in range(2, 10, 4):
+    for j in range(2, 11):
+        for k in range(i, i+4):
+            result = k * j
+            if j != 10:
+                print(f"{k} x {j} = {result:2}", end="   ")
+            else:
+                print(f"{k} x {j}= {result:2}", end="   ")
+        print()
+    print()
